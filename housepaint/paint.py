a18520ddcf8a1b3dd679216b9a96201e56edda72
nicklambourne <ndl93@live.com>
2021-05-10 21:21:09 +1000

    paramtrize paint

--- housepaint/paint.py
+++ housepaint/paint.py
@@ -4,6 +4,15 @@ from typing import Callable, List, Optional, TextIO, Union
 from housepaint.pigments import BG, FG, Style
 
 
+def parametrized(dec):
+    def layer(*args, **kwargs):
+        def repl(f):
+            return dec(f, *args, **kwargs)
+        return repl
+    return layer
+
+
+@parametrized
 def paint(
     func: Callable,
     foreground: FG = FG.DEFAULT,
@@ -11,6 +20,8 @@ def paint(
     styles: Optional[Union[Style, List[Style]]] = None,
     target: TextIO = stdout
 ) -> Callable:
+    styles = [styles] if type(styles) == Style else styles
+
     def wrapper():
         style = ''.join(map(str, styles)) if styles else ''
         print(f"{foreground}{background}{style}", end="", file=target)
